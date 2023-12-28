3ca83d584e03ecb05a2f7e4ae85f00b6d8b5ad9d
Choisab04 <155060820+Choisab04@users.noreply.github.com>
2023-12-28 18:44:08 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "Pratham@google.com"
+var accountPassword = "123"
+accountCity = "Aligarh"
+
+// accountId = 2 not allowed
+accountEmail = "Pc@gmail.com"
+accountPassword = "4444"
+accountCity = "Delhi"
+
+console.log(accountId);
+console.table([accountId, accountPassword, accountEmail, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // Treat all js code as newer version //
+
+// alert(3+3) we are using node js, not browser
+
+console.log(3+3) 
+console.log("Pratham")
+
+let name = "Pratham" //string
+let age = 21 //number
+let isLoggedIn = false //boolean
+
+// number => 2^53
+//bigint
+// string => ""
+// boolean => true/false
+//null => standalone value
+// udefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof"Pratham")
