53200f71e611d371662bed2a461428c857debd3c
Pratham Chaudhary <155060820+Choisab04@users.noreply.github.com>
2024-01-15 11:23:11 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,37 @@
+const name = "Pratham "
+const repoCount = 50
+
+console.log(name + repoCount);
+
+//String interpolation
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName  = new String('Pratham-ch')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+//(0,4) last value is not included
+console.log(newString);
+
+const anotherString = gameName.slice(-9, 4)
+console.log(anotherString);
+
+const newStringOne = "     Pratham     "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://Pratham.com/Pratham%20chaudhary"
+
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('Choisab'));
+
+console.log(gameName.split('-'));
